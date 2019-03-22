78e518189dfe1cbe8fec6361bb5de529cc9badc3
ankit <ankit@gooddoglabs.com>
2019-03-22 00:23:47 -0400

    Add more text on how to interact with the bot

--- src/components/UIJumbo.js
+++ src/components/UIJumbo.js
@@ -11,7 +11,13 @@ const UIJumbo = ({ children }) => (
       <Row>
         <Col>
           <h4> Customized insurance plans for every body I suppose... </h4>
-          <p>Styled with react-bootstrap</p>
+          <p className='lead'>Styled with react-bootstrap</p>
+          <p className='lead'>To start the conversation use either of the following lines:</p>
+          <ul>
+            <li> I would like to buy an insurance </li>
+            <li> I would like to purchase an insurance </li>
+          </ul>
+          <p className='lead'> <a href='https://github.com/ankitm123/qbot'>Github Repo</a> </p>
         </Col>
         <Col>{children}</Col>
       </Row>
